ef67dcda7345f51a56e949d79715035f1d805c6f
silvyfebryana <silvydharmafebryana@gmail.com>
2020-04-23 13:38:04 +0700

    exercise logout

--- src/views/screens/Navbar.jsx
+++ src/views/screens/Navbar.jsx
@@ -48,7 +48,7 @@ class Navbar extends React.Component {
             )
         } else {
             return (
-                null
+                <Link style={{ color: "#cc0052", fontWeight: "bold" }} to="/Login">Login</Link>
             )
         }
     }
@@ -58,7 +58,7 @@ class Navbar extends React.Component {
         return (
             <div className="d-flex justify-content-around p-3" style={{ border: "1px solid #e6e6e6", height: "60px", backgroundColor:"#e6e6e6" }}>
                 <Link style={{ color: "#cc0052", fontWeight: "bold" }} to="/Auth">Register</Link>
-                <Link style={{ color: "#cc0052", fontWeight: "bold" }} to="/Login">Login</Link>
+                {/* <Link style={{ color: "#cc0052", fontWeight: "bold" }} to="/Login">Login</Link> */}
                 <Link style={{ color: "#cc0052", fontWeight: "bold" }} to="/Account">List Account</Link>
                 <Link style={{ color: "#cc0052", fontWeight: "bold" }} to="/">Home</Link>
                 {/* <Link style={{ color: "#cc0052", fontWeight: "bold" }} to="/Input">Input Screen</Link> */}
